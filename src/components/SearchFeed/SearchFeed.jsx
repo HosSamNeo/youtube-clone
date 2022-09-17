53e96f93e029f417aa51304048a67085da5538b8
HosSamNeo <81299330+HosSamNeo@users.noreply.github.com>
2022-09-17 17:47:43 +0200

    .

--- src/components/SearchFeed/SearchFeed.jsx
+++ src/components/SearchFeed/SearchFeed.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
 import './SearchFeed.scss';
+import { Videos } from '../../components';
+import { fetchFromApi } from '../../utils/fetchFromApi';
+import { useParams } from 'react-router-dom';
 
 const SearchFeed = () => {
+  const [videos , setVideos] = useState([]);
+  const {searchTerm} = useParams();
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`)
+    .then((data) => {setVideos(data.items)})
+  }, [searchTerm])
+  
   return (
-    <div>SearchFeed</div>
+          <>
+            <h4 className='app_searchfeed-text'>
+              Search Results for <span>{searchTerm}</span> Videos
+            </h4>
+            <div>
+              <Videos videos={videos}/>
+            </div>
+          </>
   )
 }
 
-export default SearchFeed
+export default SearchFeed;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './Searchbar.scss';
 import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
 
 
 const Searchbar = () => {
+  const [searchTerm,setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+  }
   return (
-    <form>
-      <input type="text" placeholder="Search" />
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        placeholder="Search"
+        onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchTerm}
+      />
       <button><SearchIcon /></button>
   </form>
   )
