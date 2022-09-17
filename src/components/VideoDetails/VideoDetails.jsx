bfb23d30a418e7274b2b1064c2cca0cb9cfb8ce4
HosSamNeo <81299330+HosSamNeo@users.noreply.github.com>
2022-09-18 00:38:00 +0200

    .

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React , {useEffect , useState} from 'react';
+import {Link , useParams} from 'react-router-dom';
+import ReactPlayer from 'react-player';
 import './VideoDetails.scss';
+import { CheckCircle } from '@mui/icons-material';
+
+import {Video} from '../../components';
+import { fetchFromApi } from '../../utils/fetchFromApi';
 
 const VideoDetails = () => {
   return (
-    <div>VideoDetails</div>
+    <div className='app__video app__video-container'>
+
+      <div className="app__video-player">
+
+      </div>
+      
+      <div className="app__video-related">
+        
+      </div>
+
+      <div className="app__video-comments">
+
+      </div>
+      
+    </div>
   )
 }
 
